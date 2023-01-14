b0085cabda7036c3b2eaab980055252d132182f0
Agent Ebimene <ebimeneagent@gmail.com>
2023-01-14 14:50:21 +0100

    Add the functionality of fetching weather data either for a current weather details or 5 day weather forecast

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import "./App.css";
 
@@ -17,31 +17,41 @@ function App() {
   const [pressure, setPressure] = useState("");
   const [temperatureUnit, setTemperatureUnit] = useState("K");
   const [icon, setIcon] = useState(null);
+  const [weatherForecastType, setWeatherForecastType] = useState("weather");
 
   let iconUrl = `http://openweathermap.org/img/wn/${icon}@4x.png`;
 
+  const handleWeatherForecastTypeChange = (e) => {
+    setWeatherForecastType(e.target.value);
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (location.trim() === "") {
       setErrorMessage("Please enter a Location");
     }
-    const weatherResponse = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=dfcd7d85ac6f6d90550dc656ec7d05c2`
-    );
-    const weatherData = await weatherResponse.json();
-    console.log(previousTemp);
+    try {
+      const weatherResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/${
+          weatherForecastType === "weather" ? "weather" : "forecast"
+        }?q=${location}&appid=dfcd7d85ac6f6d90550dc656ec7d05c2`
+      );
+      const weatherData = await weatherResponse.json();
+      console.log(weatherData);
 
-    if (weatherResponse.status === 404) {
-      setErrorMessage("Not a real place!");
-    } else {
-      setPreviousTemp(weatherData.main.temp);
-      setHumidity(weatherData.main.humidity);
-      setPressure(weatherData.main.pressure);
-      setIcon(weatherData.weather[0].icon);
-      setDescription(weatherData.weather[0].description);
-      setCityName(weatherData.name);
-      setErrorMessage("");
-      setLoading(false);
+      // if (weatherResponse.status === 404) {
+      //   setErrorMessage("Not a real place!");
+      // } else {
+      //   setPreviousTemp(weatherData.main.temp);
+      //   setHumidity(weatherData.main.humidity);
+      //   setPressure(weatherData.main.pressure);
+      //   setIcon(weatherData.weather[0].icon);
+      //   setDescription(weatherData.weather[0].description);
+      //   setCityName(weatherData.name);
+      //   setErrorMessage("");
+      //   setLoading(false);
+      // }
+    } catch (error) {
+      console.log(error);
     }
   };
   const handleChange = (e) => {
@@ -89,6 +99,8 @@ function App() {
         handleChange={handleChange}
         location={location}
         errorMessage={errorMessage}
+        handleWeatherForecastTypeChange={handleWeatherForecastTypeChange}
+        weatherForecastType={weatherForecastType}
       />
 
       {loading ? (
--- src/components/LocationInput.js
+++ src/components/LocationInput.js
@@ -5,6 +5,8 @@ const LocationInput = ({
   handleSubmit,
   location,
   errorMessage,
+  weatherForecastType,
+  handleWeatherForecastTypeChange,
 }) => {
   return (
     <div>
@@ -19,6 +21,29 @@ const LocationInput = ({
         <p className={`error-text ${errorMessage ? "show-error" : ""}`}>
           {errorMessage}
         </p>
+        <div className="radio-buttons">
+          Forecast Type:
+          <div className="radio-container">
+            <p> Current</p>
+            <input
+              type="radio"
+              name="forecast"
+              value="weather"
+              checked={weatherForecastType === "weather"}
+              onChange={handleWeatherForecastTypeChange}
+            ></input>
+          </div>
+          <div className="radio-container">
+            <p>5 day forecast</p>
+            <input
+              name="forecast"
+              type="radio"
+              value="forecast"
+              checked={weatherForecastType === "forecast"}
+              onChange={handleWeatherForecastTypeChange}
+            ></input>
+          </div>
+        </div>
         <button type="submit">Get weather Data</button>
       </form>
     </div>
